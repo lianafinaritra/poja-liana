2c221de9d3c1d9e50826eeb9f81052da04b35d24
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-12-06 09:28:44 +0300

    fix: use probable prime function to generate prime number

--- src/main/java/com/liana/poja/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/liana/poja/endpoint/rest/controller/PrimeController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 @RestController
 @Value
 public class PrimeController {
     @GetMapping("/new-prime")
     public String getPrimeNumber() {
-        BigInteger primeNumber = new BigInteger(10000, 100, new java.util.Random());
+        BigInteger primeNumber = BigInteger.probablePrime(512, new Random(100));
 
         return primeNumber.toString();
     }
